948f1f3192768c9555026480a441ff269a3ff3a1
Luis Guzman <guzmanluis.lg@gmail.com>
2023-09-23 14:21:20 -0500

    update

--- js/slider.js
+++ js/slider.js
@@ -11,6 +11,11 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
   },
 
+  autoplay: {
+    delay: 3000, // 3000 milisegundos = 3 segundos
+    disableOnInteraction: false, // Esto asegura que la reproducción automática no se detenga cuando el usuario interactúa con el slider
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
